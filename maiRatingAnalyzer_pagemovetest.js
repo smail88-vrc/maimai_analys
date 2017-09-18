f8f878c37675ee1e79137a42009b45a2c15a8f86
Nismo Sgimera <nismo.sgimera@gmail.com>
2017-09-18 18:32:58 +0900

    Update maiRatingAnalyzer_pagemovetest.js

--- maiRatingAnalyzer_pagemovetest.js
+++ maiRatingAnalyzer_pagemovetest.js
@@ -4,11 +4,6 @@ function top2mlist()
 	var eroot = document.getElementsByClassName('header');
 	eroot = eroot.firstElementChild;
 	
-	while(eroot.nodeName="center")
-	{
-		eroot = eroot.nextElementSibling;
-	}
-	
 	confirm(eroot);
 }
 
