087e4773c9e6852ae801d88d6fdfeaac149d7f2b
Nismo Sgimera <nismo.sgimera@gmail.com>
2017-09-24 15:53:42 +0900

    Update maiRatingAnalyzer_pagemovetest.js

--- maiRatingAnalyzer_pagemovetest.js
+++ maiRatingAnalyzer_pagemovetest.js
@@ -1,13 +1,21 @@
 javascript:
-function top2mlist()
+
+function address_musiclist(diff)
 {
-	var eroot = document.getElementsByClassName('header');
-	eroot = eroot.firstElementChild;
-	
-	confirm(eroot);
+	var eroot = document.getElementsByTagName('a');
+	for(var i=0; i<eroot.length; i++)
+	{
+		var url=eroot[i].getAttribute('href');
+		if(url.indexOf("music.html") == 0)
+		{
+			return url+"&d="+diff;
+		}
+	}
 }
 
 (function()
 {
-	top2mlist();
+	var url = address_musiclist(5);
+	var master_window = window.open(url);
+	confirm(master_window);
 })()
