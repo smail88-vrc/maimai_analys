34aaa56851ce823bcb35b67d0e459f366426dfb5
Nismo Sgimera <nismo.sgimera@gmail.com>
2017-10-05 01:54:21 +0900

    データ分割テスト

--- maiRatingAnalyzer_pagemovetest.js
+++ maiRatingAnalyzer_pagemovetest.js
@@ -118,7 +118,7 @@ function get_levellist()
 				var lvlist_len = inner_level.length;
 				for(var i=0; i<lvlist_len; i++)
 				{
-					inner_lv.push([].push(inner_level[i][0]).push(inner_level[i][1]).push(inner_leve[i][2]));
+					inner_lv.push([inner_level[i][0], inner_level[i][1], inner_leve[i][2]]);
 				}
 		       });
 	return;
