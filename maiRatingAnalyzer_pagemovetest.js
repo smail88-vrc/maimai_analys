1e3053fcd923199cddc578d2fe85b7d47aef3b49
Nismo Sgimera <nismo.sgimera@gmail.com>
2017-10-09 15:34:16 +0900

    計算復活

--- maiRatingAnalyzer_pagemovetest.js
+++ maiRatingAnalyzer_pagemovetest.js
@@ -643,13 +643,13 @@ function data2rating()
 				rate_values:[0,	0, 0],
 				music_rate : 0
 			});
-//			datalist.rate_values[0] = arch2rate_10000(datalist[i].achive[0], datalist[i].lv[0]);
-//			datalist.rate_values[1] = arch2rate_10000(datalist[i].achive[1], datalist[i].lv[1]);
-//			if(datalist[i].lv[2] != "")
-//			{
-//				datalist.rate_values[2] = arch2rate_10000(datalist[i].achive[2], datalist[i].lv[2]);
-//			}
-//			datalist[i].musicrate = Math.max(datalist[i].rate_values);
+			datalist.rate_values[0] = arch2rate_10000(datalist[i].achive[0], datalist[i].lv[0]);
+			datalist.rate_values[1] = arch2rate_10000(datalist[i].achive[1], datalist[i].lv[1]);
+			if(datalist[i].lv[2] != "")
+			{
+				datalist.rate_values[2] = arch2rate_10000(datalist[i].achive[2], datalist[i].lv[2]);
+			}
+			datalist[i].musicrate = Math.max(datalist[i].rate_values);
 			
 			lvlist_count++;
 		}
