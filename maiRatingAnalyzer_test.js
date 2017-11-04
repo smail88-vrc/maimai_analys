44008be7b1fe4e3dfc8cecd9fd46f9499501a191
Nismo Sgimera <nismo.sgimera@gmail.com>
2017-11-04 23:13:13 +0900

    Create maiRatingAnalyzer_test.js

---
+++ maiRatingAnalyzer_test.js
@@ -0,0 +1,27 @@
+javascript:
+(function()
+	{
+		var d=document;
+		var i=location.hostname.indexOf('maimai-net.com');
+		if(i!=-1)
+		{
+			var url='https://sgimera.github.io/mai_RatingAnalyzer/mai_inner_level.js';
+			var e=d.createElement('script');
+			e.src=url;
+ 			if(e == d.getElementsByTagName('head')[0].appendChild(e));
+			{
+ 				url='https://sgimera.github.io/mai_RatingAnalyzer/maiRatingAnalyzer_test_body.js';
+				e=d.createElement('script');
+				e.src=url;
+ 				d.getElementsByTagName('head')[0].appendChild(e);
+			}
+ 		}
+		else
+		{
+			if(confirm('Do you open maimai.net?'))
+			{
+				window.open('https://maimai-net.com/');
+			}
+		}
+	})();
+void(0);
