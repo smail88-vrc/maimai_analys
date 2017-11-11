ad4de5c4d686015baabf9dd150884670b0fe1073
Nismo Sgimera <nismo.sgimera@gmail.com>
2017-11-11 18:32:59 +0900

    Update maiRatingAnalyzer_test.js

--- maiRatingAnalyzer_test.js
+++ maiRatingAnalyzer_test.js
@@ -1,12 +1,22 @@
 javascript:
-(function(){
-	var i=0,s;
-	var urls=["https://sgimera.github.io/mai_RatingAnalyzer/scripts/mai_inner_level.js",
-			"https://sgimera.github.io/mai_RatingAnalyzer/scripts/maiRatingAnalyzer_body.js"];
-  for(i=0;i<urls.length;i++){
-    s= document.createElement("script");
-    s.src=urls[i];
-    s.onload=function(){return;};
-    document.body.appendChild(s);
-  };
-})();void(0);
+(
+	function(j,f)
+	{
+		j=['https://sgimera.github.io/mai_RatingAnalyzer/scripts/mai_inner_level.js',
+			'https://sgimera.github.io/mai_RatingAnalyzer/scripts/maiRatingAnalyzer_body.js'];
+		f=function(s,u)
+		{
+			if(j.length==0)
+			{
+				return;
+			}
+			u=j.shift()+'?'+Date.now();
+			s=document.createElement('script');
+			document.body.appendChild(s);
+			s.charset='UTF-8';
+			s.addEventListener('load',f);
+			s.src=u;
+		};
+		(document.readyState=='loading')?document.addEventListener('DOMContentLoaded',f):f();
+	}
+)();
