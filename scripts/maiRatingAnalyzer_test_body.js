06365195c49479df07d6497e43dd02ab1269e6a6
Nismo Sgimera <nismo.sgimera@gmail.com>
2017-11-11 23:30:47 +0900

    Update maiRatingAnalyzer_test_body.js

--- scripts/maiRatingAnalyzer_test_body.js
+++ scripts/maiRatingAnalyzer_test_body.js
@@ -203,7 +203,7 @@ function print_result2(golliramode)
 	result_str += "<style type='text/css'>";
 	result_str += "\ttable { border-collapse: collapse; }";
 	result_str += "<\/style>";
-	result_str += "<style type ='text/css' src='./css/mai_rating.css'><\/style>
+	result_str += "<style type ='text/css' src='./css/mai_rating.css'><\/style>";
 	result_str += "<\/head>";
 	result_str += "<table border=1 align=\"center\">";
 	
