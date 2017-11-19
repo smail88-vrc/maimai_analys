0684dfc7e1c7326d1e3ddb2df59c0f21f61ec7e1
Nismo Sgimera <nismo.sgimera@gmail.com>
2017-11-19 22:26:18 +0900

    レート値計算分離

--- scripts/maiRatingAnalyzer_test_body.js
+++ scripts/maiRatingAnalyzer_test_body.js
@@ -9,94 +9,6 @@ var best_ave=0, best_limit=0, hist_limit=0;
 var expect_max=0, best_rating=0, top_rate=0, recent_rating=0, hist_rating=0, best_left=0, hist_left=0;
 var tweet_rate_str="", 	tweet_best_str="";
 
-function diff2s(difficallity)
-{
-	var tmp = diff2tmp(difficallity),retval=0;
-	switch(Math.floor(tmp))
-	{
-		case 13:
-			retval = tmp+0.5;
-			break;
-		case 12:
-			retval = 12.00+(tmp*1-12.00)*(3/2);
-			break;
-		default:
-			retval = tmp;
-			break;
-	}
-	return Math.round(retval*100)/100;
-}
-
-function diff2sss(difficallity)
-{
-	var tmp=diff2tmp(difficallity), retval=0;
-	switch(Math.floor(tmp))
-	{
-		case 13:
-			retval = tmp*1+3.0;
-			break;
-		case 12:
-			retval = tmp*2-10.00;
-			break;
-		case 11:
-			retval = 2.00+tmp*1;
-			break;
-		case 10:
-			retval = 7.50+tmp/2;
-			break;
-		case 9:
-		case 8:
-			retval = 2.50+tmp*1;
-			break;
-		case 7:
-		default:
-			retval = 6.50+tmp/2;
-			break;
-	}
-	return Math.round(retval*100)/100;
-}
-
-function rate_XtoY(basis, max, gap, n)
-{
-	return basis+(max-basis)*n/gap
-}
-
-function arch2rate_10000(achievement, difficallity)
-{
-	var temp = 0;
-
-		var rate_sss = Math.round(10000*diff2sss(difficallity));
-		var rate_ss = rate_sss - 10000;
-		var rate_s = Math.round(10000*diff2s(difficallity));
-		var diff10000 = Math.round(10000*diff2tmp(difficallity));
-		var achi_100 = Math.round(achievement*100);
-		if(achi_100 >= 10000) {
-			temp = rate_sss
-		} else if (achi_100 >= 9900) {
-			temp = rate_XtoY(rate_ss,     rate_sss-2500,  100, achi_100-9900);
-		} else if (achi_100 >= 9700) {
-			temp = rate_XtoY(rate_s,      rate_ss-2500,   200, achi_100-9700);
-		} else if (achi_100 >= 9400) {
-			temp = rate_XtoY(diff10000-15000, rate_s-10000,   300, achi_100-9400);
-		} else if (achi_100 >= 9000) {
-			temp = rate_XtoY(diff10000-20000, diff10000-15000,  400, achi_100-9000);
-		} else if (achi_100 >= 8000) {
-			temp = rate_XtoY(diff10000-30000, diff10000-25000, 1000, achi_100-8000);
-		} else if (achi_100 >= 6000) {
-			temp = rate_XtoY(diff10000*0.4, diff10000-40000, 2000, achi_100-6000);
-		} else if (achi_100 >= 4000) {
-			temp = rate_XtoY(diff10000*0.2, diff10000*0.4, 2000, achi_100-4000);
-		} else if (achi_100 >= 2000) {
-			temp = rate_XtoY(diff10000*0.1, diff10000*0.2, 1000, achi_100-2000);
-		} else if (achi_100 >= 1000) {
-			temp = rate_XtoY(0,           diff10000*0.1, 1000, achi_100-1000);
-		} else {
-			temp = 0;
-		}
-	temp -= temp % 1.0;
-	return temp;
-}
-
 function get_nextpage_address(j,html,diff)	//次の楽曲リストページを探す
 {
 	var nextaddr="";
@@ -170,9 +82,9 @@ function data2rating(golliramode)
 				music_rate : 0
 			});
 			datalist[i].rate_values[0] =
-				(golliramode == 0)?arch2rate_10000(datalist[i].achive[0], datalist[i].lv[0]):0;
-			datalist[i].rate_values[1] = arch2rate_10000(datalist[i].achive[1], datalist[i].lv[1]);
-			datalist[i].rate_values[2] = arch2rate_10000(datalist[i].achive[2], datalist[i].lv[2]);
+				(golliramode == 0)?mra_arch2rate_10000(datalist[i].achive[0], datalist[i].lv[0]):0;
+			datalist[i].rate_values[1] = mra_arch2rate_10000(datalist[i].achive[1], datalist[i].lv[1]);
+			datalist[i].rate_values[2] = mra_arch2rate_10000(datalist[i].achive[2], datalist[i].lv[2]);
 			datalist[i].music_rate = Math.max.apply(null, datalist[i].rate_values);
 			
 			lvlist_count++;
