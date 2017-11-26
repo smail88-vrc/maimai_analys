e9d98ae954cdb81be7e7f4181ef721d2a4161428
Nismo Sgimera <nismo.sgimera@gmail.com>
2017-11-26 16:27:59 +0900

    Create get_friend_list.js

---
+++ get_friend_list.js
@@ -0,0 +1,23 @@
+javascript:
+(
+	function(j,f)
+	{
+		j=['https://sgimera.github.io/mai_RatingAnalyzer/scripts/mai_inner_level.js',
+		   'https://sgimera.github.io/mai_RatingAnalyzer/scripts/calc_rating.js',
+		   'https://sgimera.github.io/mai_RatingAnalyzer/scripts/maiRatingAnalyzer_test_body.js'];
+		f=function(s,u)
+		{
+			if(j.length==0)
+			{
+				return;
+			}
+			u=j.shift()+'?'+Date.now();
+			s=document.createElement('script');
+			document.body.appendChild(s);
+			s.charset='UTF-8';
+			s.addEventListener('load',f);
+			s.src=u;
+		};
+		(document.readyState=='loading')?document.addEventListener('DOMContentLoaded',f):f();
+	}
+)();
