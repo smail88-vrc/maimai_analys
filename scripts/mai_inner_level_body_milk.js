a95b237588307cde2ad9070fabcb610626804933
Nismo Sgimera <nismo.sgimera@gmail.com>
2017-12-16 03:17:48 +0900

    Update mai_inner_level_body_milk.js

--- scripts/mai_inner_level_body_milk.js
+++ scripts/mai_inner_level_body_milk.js
@@ -310,7 +310,7 @@ mra_max_rating +=
 	print_result_sub("BEST枠", (best_rating.toFixed(2)) + "<br>(" + (best_left.toFixed(2)) + ")", "(上位30曲の合計)/44<br>()は+0.01する為の必要レート");
 mra_max_rating += print_result_sub("RECENT枠", (recent_rating.toFixed(2)), "レート値1位を10回達成");
 mra_max_rating +=
-	print_result_sub("HISTORY枠", (hist_rating.toFixed(2)) + "<br>(" + (hist_left.toFixed(2)) + ")", "(上位434曲の合計)/(473*44/4)<br>()は+0.01する為の必要レート");
+	print_result_sub("HISTORY枠", (hist_rating.toFixed(2)) + "<br>(" + (hist_left.toFixed(2)) + ")", "(上位473曲の合計)/(473*44/4)<br>()は+0.01する為の必要レート");
 
 mra_max_rating += "<\/table>";
 
