77c595293e1f3eaccf599e4501041405db8e5555
Nismo Sgimera <nismo.sgimera@gmail.com>
2017-12-24 17:52:24 +0900

    Update maiRatingAnalyzer_test_body.js

--- scripts/maiRatingAnalyzer_test_body.js
+++ scripts/maiRatingAnalyzer_test_body.js
@@ -72,23 +72,7 @@ function true_achive(score, score100per)
 	
 function sort_condition(a,b)
 {
-	var lv_a, lv_b, achi_a, achi_b;
-	console.log("music_rate : " + b.music_rate + " vs " + a.music_rate);
-	if(b.music_rate != a.music_rate)
-	{
-		return b.music_rate > a.music_rate;
-	}
-	lv_a=Math.max.apply(null, a.lv.map(mra_diff2tmp));
-	lv_b=Math.max.apply(null, b.lv.map(mra_diff2tmp));
-	console.log("level : " +lv_a + " vs " + lv_b);
-//	if(lv_a != lv_b)
-//	{
-		return lv_b > lv_a;
-//	}
-//	achi_a=Math.max.apply(null, a.achive);
-//	achi_b=Math.max.apply(null, b.achive);
-//	console.log("achivement : " + achi_a + " vs " + achi_b);
-//	return achi_b > achi_a;
+	return b.music_rate >= a.music_rate;
 }
 	
 function data2rating(golliramode)
