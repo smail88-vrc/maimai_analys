5405d66f2f129b7df26c77d1670a1618da70fee1
Nismo Sgimera <nismo.sgimera@gmail.com>
2017-12-24 21:22:16 +0900

    ソート条件追加

--- scripts/maiRatingAnalyzer_test_body.js
+++ scripts/maiRatingAnalyzer_test_body.js
@@ -73,22 +73,22 @@ function true_achive(score, score100per)
 function sort_condition(a,b)
 {
 	var lv_a, lv_b, achi_a, achi_b;
-	console.log("music_rate : " + b.music_rate + " vs " + a.music_rate);
+//	console.log("music_rate : " + b.music_rate + " vs " + a.music_rate);
 	if(b.music_rate != a.music_rate)
 	{
 		return b.music_rate - a.music_rate;
 	}
 	lv_a=Math.max.apply(null, a.lv.map(mra_diff2tmp));
 	lv_b=Math.max.apply(null, b.lv.map(mra_diff2tmp));
-	console.log("level : " +lv_a + " vs " + lv_b);
-//	if(lv_a != lv_b)
-//	{
+//	console.log("level : " +lv_a + " vs " + lv_b);
+	if(lv_a != lv_b)
+	{
 		return lv_b - lv_a;
-//	}
-//	achi_a=Math.max.apply(null, a.achive);
-//	achi_b=Math.max.apply(null, b.achive);
+	}
+	achi_a=Math.max.apply(null, a.achive);
+	achi_b=Math.max.apply(null, b.achive);
 //	console.log("achivement : " + achi_a + " vs " + achi_b);
-//	return achi_b > achi_a;
+	return achi_b - achi_a;
 }	
 function data2rating(golliramode)
 {
@@ -354,7 +354,7 @@ function tweet_best(id)
 function datalist_recalc()
 {
 	var listlength=datalist.length, tmplv="", count=0;
-	console.log(listlength);
+//	console.log(listlength);
 	
 	for(var i=0; i<listlength; i++)
 	{
