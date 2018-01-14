cf51333558fa84fff8587add1f6e14bebfed5d70
Nismo Sgimera <nismo.sgimera@gmail.com>
2018-01-14 19:54:07 +0900

    Update mai_inner_level_body_murasakip.js

--- scripts/mai_inner_level_body_murasakip.js
+++ scripts/mai_inner_level_body_murasakip.js
@@ -175,7 +175,7 @@ rating_table = rating_table.sort(function(a,b){return b-a;}).map(String);
 s_rating=calc_rating(rating_table.map(function(x){return mra_arch2rate_100(0.97,x);}), false);
 ss_rating=calc_rating(rating_table.map(function(x){return mra_arch2rate_100(0.995,x);}), false);
 sss_rating=calc_rating(rating_table.map(function(x){return mra_arch2rate_100(1,x);}), true);
-console.log(rating_table.map(function(x){return mra_arch2rate_100(0.97,x);}));
+console.log(rating_table.map(function(x){return mra_arch2rate_100(1,x);}));
 
 mra_evaluated += "<table border=1>";
 mra_evaluated += mra_level_lavel("Level 13");
