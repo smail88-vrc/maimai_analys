5aa9d4a4918d4213e385f4991fd4685ae07af710
Nismo Sgimera <nismo.sgimera@gmail.com>
2018-01-27 15:56:49 +0900

    時間表示追加テスト

--- scripts/maiRatingAnalyzer_test_body.js
+++ scripts/maiRatingAnalyzer_test_body.js
@@ -190,8 +190,12 @@ function print_result(golliramode, homeaddr)
 
 	result_str += "<table border=1 align=\"center\">";
 	
+	var today = new Date();
+	
 	result_str += "<tr>";
-	result_str += "<th colspan=3 bgcolor=\"\#000000\"><font color=\"\#ffffff\">基本データ<\/font><\/th>";
+	result_str += "<th colspan=3 bgcolor=\"\#000000\"><font color=\"\#ffffff\">基本データ<br>";
+	result_str += today.getFullYear() + "/" + (today.getMonth()+1) + "/" + today..getDate() + " ";
+	result_str += today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + "現在<\/font><\/th>";
 	result_str += "<\/tr>";
 	
 	result_str += "<tr>";
