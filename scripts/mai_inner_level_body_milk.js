c5de81e668b23a1ed75437201d49b2398532064a
Nismo Sgimera <nismo.sgimera@gmail.com>
2018-01-27 18:32:37 +0900

    Update mai_inner_level_body_milk.js

--- scripts/mai_inner_level_body_milk.js
+++ scripts/mai_inner_level_body_milk.js
@@ -61,9 +61,9 @@ function print_result_sub(title, value, explain)
 }
 
 		
-var lv136="", lv135="", lv134="", lv133="", lv132="", lv131="", lv130="", lv13_="";
-var lv129="", lv128="", lv127="", lv12p="";
-var lv126="", lv125="", lv124="", lv123="", lv122="", lv121="", lv120="", lv12_="";
+var lv136="", lv135="", lv134="", lv133="", lv132="", lv131="", lv130="", lv13_="", lv13h="";
+var lv129="", lv128="", lv127="", lv12p="", lv12ph="";
+var lv126="", lv125="", lv124="", lv123="", lv122="", lv121="", lv120="", lv12_="", lv12h="";
 var lv119="", lv118="", lv117="", lv11p="";
 var lv116="", lv115="", lv114="", lv113="", lv112="", lv111="", lv110="", lv11_="";
 var lv109="", lv108="", lv107="", lv10p="";
@@ -162,7 +162,7 @@ for(var i=0; i<mlist_length; i++)
 				if(maimai_inner_lv[i].levels[lv].slice(0,1)=="(")
 				{ lv13_ += tmpstr; }	//未検証
 				else
-				{ lv130 += tmpstr; }	//検証済み
+				{ lv13h += tmpstr; }	//検証済み
 				continue;
 			case "12.9":
 			case "12.8":
@@ -170,7 +170,7 @@ for(var i=0; i<mlist_length; i++)
 				if(maimai_inner_lv[i].levels[lv].slice(0,1)=="(")
 				{ lv12p += tmpstr; }	//未検証
 				else
-				{ lv127 += tmpstr; }	//検証済み
+				{ lv12ph += tmpstr; }	//検証済み
 				continue;
 			case "12.6":
 			case "12.5":
@@ -182,7 +182,7 @@ for(var i=0; i<mlist_length; i++)
 				if(maimai_inner_lv[i].levels[lv].slice(0,1)=="(")
 				{ lv12_ += tmpstr; }	//未検証
 				else
-				{ lv120 += tmpstr; }	//検証済み
+				{ lv12ph += tmpstr; }	//検証済み
 				continue;
 			case "11.9":
 			case "11.8":
@@ -260,14 +260,14 @@ mra_evaluated += "<table border=1>";
 mra_evaluated += mra_level_lavel("Level 13");
 //mra_evaluated += mra_add_musiclevel_list(["13.6", "13.5", "13.4", "13.3", "13.2", "13.1", "13.0"],
 //			[lv136, lv135, lv134, lv133, lv132, lv131, lv130]);
-mra_evaluated += "<tr><th>13<\/th> <td>" + lv130 + "<\/td><\/tr>";
+mra_evaluated += "<tr><th>13<\/th> <td>" + lv13h + "<\/td><\/tr>";
 mra_evaluated += mra_level_lavel("Level 12+");
 //mra_evaluated += mra_add_musiclevel_list(["12.9", "12.8", "12.7"], [lv129, lv128, lv127]);
-mra_evaluated += "<tr><th>12+<\/th> <td>" + lv127 + "<\/td><\/tr>";
+mra_evaluated += "<tr><th>12+<\/th> <td>" + lv12ph + "<\/td><\/tr>";
 mra_evaluated += mra_level_lavel("Level 12");
 //mra_evaluated += mra_add_musiclevel_list(["12.6", "12.5", "12.4", "12.3", "12.2", "12.1", "12.0"],
 //			[lv126, lv125, lv124, lv123, lv122, lv121, lv120]);
-mra_evaluated += "<tr><th>12<\/th> <td>" + lv120 + "<\/td><\/tr>";
+mra_evaluated += "<tr><th>12<\/th> <td>" + lv12h + "<\/td><\/tr>";
 mra_evaluated += mra_level_lavel("Level 11+");
 mra_evaluated += mra_add_musiclevel_list(["11.9", "11.8", "11.7"], [lv119, lv118, lv117]);
 mra_evaluated += mra_level_lavel("Level 11");
