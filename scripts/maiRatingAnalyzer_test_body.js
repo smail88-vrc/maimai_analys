1b2f6714adea6d945a78f28d8291821e8329b322
Nismo Sgimera <nismo.sgimera@gmail.com>
2018-01-27 16:19:16 +0900

    Update maiRatingAnalyzer_test_body.js

--- scripts/maiRatingAnalyzer_test_body.js
+++ scripts/maiRatingAnalyzer_test_body.js
@@ -192,7 +192,7 @@ function print_result(golliramode, homeaddr)
 	
 	var today = new Date();
 	var data_str = today.getFullYear() + "\/" + (today.getMonth()+1) + "\/" + today.getDate() + " ";
-	data_str += (("0"+today.getHours()).slice(-2)) + ":" + (("0"+today.getDate()).slice(-2)) + ":" + (("0"+today.getSeconds()).slice(-2));
+	data_str += (("0"+today.getHours()).slice(-2)) + ":" + (("0"+today.getMinutes()).slice(-2)) + ":" + (("0"+today.getSeconds()).slice(-2));
 	
 	result_str += "<tr>";
 	result_str += "<th colspan=3 bgcolor=\"\#000000\"><font color=\"\#ffffff\">基本データ<br>";
