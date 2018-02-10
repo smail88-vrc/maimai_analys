425162c2a4dcd337ffd0f8768d2b44f4ab8f78db
Nismo Sgimera <nismo.sgimera@gmail.com>
2018-02-10 23:40:54 +0900

    Update maiRatingAnalyzer_test_body.js

--- scripts/maiRatingAnalyzer_test_body.js
+++ scripts/maiRatingAnalyzer_test_body.js
@@ -3,7 +3,7 @@ javascript:
 {
 
 var ex_list=[], ma_list=[], re_list=[], datalist=[], addr="", your_id="", your_rating="";
-var hashtag = "%e8%88%9e%e3%83%ac%e3%83%bc%e3%83%88%e8%a7%a3%e6%9e%90";	// 舞レート解析test
+var hashtag = "%e8%88%9e%e3%83%ac%e3%83%bc%e3%83%88%e8%a7%a3%e6%9e%90test";	// 舞レート解析test
 var mra_update_algorithm = "2018.01.27";
 
 var best_ave=0, best_limit=0, hist_limit=0;
