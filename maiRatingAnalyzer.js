816fa71052446873edef5bf2b7cd9999b113f5a7
Nismo Sgimera <nismo.sgimera@gmail.com>
2018-03-09 00:25:46 +0900

    再公開

--- maiRatingAnalyzer.js
+++ maiRatingAnalyzer.js
@@ -1,1 +1,24 @@
-javascript:alert('--舞レート解析・あならいざもどき--\n\n公開停止中です。復帰まで少々お待ちを。');
+javascript:
+(
+	function(j,f,dom)
+	{
+		j=['mai_inner_level_milk2.js',
+		   'calc_rating_test.js',
+		   'maiRatingAnalyzer_body.js'];
+		dom='https://sgimera.github.io/mai_RatingAnalyzer/scripts/';
+		f=function(s,u)
+		{
+			if(j.length==0)
+			{
+				return;
+			}
+			u=dom+j.shift()+'?'+Date.now();
+			s=document.createElement('script');
+			document.body.appendChild(s);
+			s.charset='UTF-8';
+			s.addEventListener('load',f);
+			s.src=u;
+		};
+		(document.readyState=='loading')?document.addEventListener('DOMContentLoaded',f):f();
+	}
+)();
