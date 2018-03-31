88dfe48c1d007b79493366b302a9d0f9b59dddf4
Nismo Sgimera <nismo.sgimera@gmail.com>
2018-03-31 12:05:19 +0900

    Create comp_test.js

---
+++ comp_test.js
@@ -0,0 +1,22 @@
+javascript:
+(
+	function(j,f,dom)
+	{
+		j=['complist_test.js'];
+		dom='https://sgimera.github.io/mai_RatingAnalyzer/scripts/';
+		f=function(s,u)
+		{
+			if(j.length==0)
+			{
+				return;
+			}
+			u=dom+j.shift()+'?'+Date.now();
+			s=document.createElement('script');
+			document.body.appendChild(s);
+			s.charset='UTF-8';
+			s.addEventListener('load',f);
+			s.src=u;
+		};
+		(document.readyState=='loading')?document.addEventListener('DOMContentLoaded',f):f();
+	}
+)();
