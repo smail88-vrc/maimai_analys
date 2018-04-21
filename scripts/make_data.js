e4792bceca8cd1fd785b5b0f27e3efb710208e5f
Nismo Sgimera <nismo.sgimera@gmail.com>
2018-04-21 19:55:58 +0900

    Update make_data.js

--- scripts/make_data.js
+++ scripts/make_data.js
@@ -190,7 +190,7 @@ document.write('<br>');
 
 document.write('var mlist_length=' + mlist_length + ';<br>');
 document.write('var mra_update_mlist="' + mra_update_mlist + '";<br>');
-document.write('var mra_update_llist="' + mra_update_llist + ';"<br>');
+document.write('var mra_update_llist="' + mra_update_llist + '";<br>');
 
 document.write('var s_rating=' + s_rating.toFixed(2) + ';<br>');
 document.write('var ss_rating=' + ss_rating.toFixed(2) + ';<br>');
