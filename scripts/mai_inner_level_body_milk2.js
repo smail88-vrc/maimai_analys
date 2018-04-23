7fdafbe6c8bb6f2eb8d56ccd9b5bf6733f0c3f4e
Nismo Sgimera <nismo.sgimera@gmail.com>
2018-04-23 22:40:20 +0900

    Update mai_inner_level_body_milk2.js

--- scripts/mai_inner_level_body_milk2.js
+++ scripts/mai_inner_level_body_milk2.js
@@ -9,17 +9,6 @@ function get_ratingrank(rating)
 	(rating>=1)?("mai_blue"):("mai_white");
 }
 
-function mra_add_musiclevel_unknown_list(lv_list, m_list)
-{
-	var liststr="";
-	for(var i=0; i<lv_list.length; i++)
-	{
-		liststr += "<tr><th>" + lv_list[i] + "<\/th>";
-		liststr += "<td>" + ((m_list[i]=="")?("（全部検証済）"):(m_list[i])) + "<\/td><\/tr>";
-	}
-	
-	return liststr;
-}
 
 function mra_add_musiclevel_list(lv_list, m_list)
 {
@@ -44,16 +33,18 @@ function mra_level_lavel(lv_str)
 
 function inner_level_milk_input()
 {
-	var not_eval_lv_list=["13", "12+", "12", "11+", "11", "10+", "10", "9+", "9"];
-	var not_eval_msc_list=[lv13_, lv12p, lv12_, lv11p, lv11_, lv10p, lv10_, lv09p, lv09_];
+	var not_eval_lv_list=["13", "12+", "12", "11+", "11", "10+", "10", "9+", "9", "8+"];
+	var not_eval_msc_list=[lv13_, lv12p, lv12_, lv11p, lv11_, lv10p, lv10_, lv09p, lv09_, lv08p];
 
-	for(var i=0; i<9; i++)
+	$('#not_eval_date')[0].innerText=mra_update_mlist + "追加譜面までの未検証譜面";
+	for(var i=0; i<10; i++)
 	{
 		$('[id=not_eval_level]')[i].innerText=not_eval_lv_list[i];
 		$('[id=not_eval_music]')[i].innerText=
 			(not_eval_msc_list[i]=="")?("（全部検証済）"):(not_eval_msc_list[i]);
 	}
 
+/*
 	mra_evaluated += "<table border=1>";
 	mra_evaluated += "<tr><th colspan=2>" + mra_update_llist + "時点での検証済譜面<\/th><\/tr>";
 	mra_evaluated += mra_level_lavel("Level 13");
@@ -79,8 +70,8 @@ function inner_level_milk_input()
 	mra_evaluated += mra_level_lavel("Level 8+");
 	mra_evaluated += mra_add_musiclevel_list(lv8plus, lv8p_rslt);
 	mra_evaluated += "<\/table>"
-
-//	$('#mra_not_evaluated')[0].innerText=mra_not_evaluated;
+*/
+	
 //	$('#mra_evaluated')[0].innerText=mra_evaluated;
 	$('#mlist_length')[0].innerText=mlist_length;
 	$('#s_rating')[0].innerText=s_rating.toFixed(2);
