2a991e8530a55d77532a5c5ef4ceac5336960bc9
Nismo Sgimera <nismo.sgimera@gmail.com>
2018-05-03 01:10:18 +0900

    Update otameshi_body.js

--- scripts/otameshi_body.js
+++ scripts/otameshi_body.js
@@ -25,9 +25,9 @@ var clist=[], ranklist=[], complist=[];	// コレクション系
 var tweet_rate_str="", 	tweet_best_str=""; // ツイート系
 var friendmode = false; // 動作モード系
 
-var hashtag = "%e8%88%9e%e3%83%ac%e3%83%bc%e3%83%88%e8%a7%a3%e6%9e%90";	// 舞レート解析test
+var hashtag = "%e8%88%9e%e3%83%ac%e3%83%bc%e3%83%88%e8%a7%a3%e6%9e%90test";	// 舞レート解析test
 var mainet_dom = 'https://maimai-net.com/maimai-mobile/';
-var mra_update_algorithm = "2018.05.01";
+var mra_update_algorithm = "2018.05.02";
 var max_play_hist=50;
 
 var music_count=maimai_inner_lv.length;
@@ -399,7 +399,7 @@ function collection_filter(collection_list)
 		tmpidx=-1;
 		while(tmpidx==-1 && lnum.length!=0)
 			tmpidx=lnum.shift();
-		ranklist.push((tmpidx!=-1)?"<img src='"+ collection_list[tmpidx].addr + "' width=105>":"");
+		ranklist.push((tmpidx!=-1)?"<img src='"+ collection_list[tmpidx].addr + "' alt=" + collection_list[tmpidx].name + " width=105>":"");
 	}
 
 	// 初代のcomp称号
@@ -424,7 +424,8 @@ function collection_filter(collection_list)
 		lnum = c_comp_plate_list[i].map(function(x){return collection_list.map(function(y){return y.name;}).indexOf(x);});
 		if(lnum[0]!=-1) lnum[3]=-1; // 舞舞なら極は表示しない
 		if(lnum[1]!=-1) {lnum[2]=-1; lnum[3]=-1;} // 神なら将、極は表示しない
-		complist.push(lnum.map(function(x){ return (x==-1)?"":("<img src='"+ collection_list[x].addr + "' width=105>")}).join(""));
+		complist.push(lnum.map(function(x){ return (x==-1)?"":
+			("<img src='"+ collection_list[x].addr + "' alt=" + collection_list[tmpidx].name + " width=105>")}).join(""));
 	}
 	return;
 }
