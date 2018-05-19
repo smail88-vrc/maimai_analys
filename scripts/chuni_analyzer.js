065680ba270572ffa69e88075d93646254a5f7e8
Nismo Sgimera <nismo.sgimera@gmail.com>
2018-05-19 19:59:46 +0900

    Update chuni_analyzer.js

--- scripts/chuni_analyzer.js
+++ scripts/chuni_analyzer.js
@@ -72,6 +72,41 @@ function get_musicname(addr, diff, array)
 	return;
 }
 
+function score2eval(score)
+{
+	return (score>=1007500)?{rank:'SSS', achi:(score-1007500)*0.75/ 2500}:
+	       (score>=1005000)?{rank:'SS+', achi:(score-1005000)*0.5 / 2500}:
+	       (score>=1000000)?{rank:'SS' , achi:(score-1000000)*0.5 / 5000}:
+	       (score>= 975000)?{rank:'S'  , achi:(score- 975000)/ 25000}:
+	       (score>= 950000)?{rank:'AAA', achi:(score- 950000)*1.5 /25000}:
+	       (score>= 925000)?{rank:'AA' , achi:(score- 925000)*1.5/ 25000}:
+	       (score>= 900000)?{rank:'A'  , achi:(score- 900000)*2.0/ 25000}:
+	       (score>= 800000)?{rank:'BBB', achi:(score- 800000)/100000}:
+	       (score>= 500000)?{rank:'C'  , achi:(score- 500000)/100000}:
+	       {rank:'D', achi:score/500000};
+}
+
+function eval2pdata(l,d)
+{
+	switch(d.rank)
+	{
+		case 'SSS':
+		case 'SS+':
+		case 'SS':
+		case 'S':
+		case 'AAA':
+		case 'AA':
+		case 'A':
+			return l + '/ ' + d.rank + ' / +' + (Math.floor(d.achi * 100)/100).foFixed(2);
+		case 'BBB':
+		case 'C':
+		case 'D' :
+			return l + '/ ' + d.rank + ' / ' + (Math.floor(d.achi *10000)/100).toFixed(2) + '%';
+		default:
+			return "";
+	}
+}
+
 //メインはここから
 get_musicname(chuni_dom + 'MusicRanking.html', 'master', mname_list);
 get_scoredata(chuni_dom + 'MusicGenre.html', 'master', ma_list);
@@ -84,8 +119,11 @@ var scoretable="";
 scoretable += "<table border=1 align=center>";
 for(var i=0; i<mname_list.length; i++)
 {
-	scoretable += "<tr><th>" + mname_list[i] + "</th><td>" + ma_list[i].score + "</td><td>" + ex_list[i].score + "</td>";
-	scoretable += "<td>" + adv_list[i].score + "</td><td>" + ba_list[i].score + "</td></tr>";
+	scoretable += "<tr><th>" + chuni_music_list[i].name + "</th>";
+	scoretable += "<td>" + eval2pdata(chuni_music_list[i].lv[3], score2eval(ma_list[i].score)) + "</td>";
+	scoretable += "<td>" + eval2pdata(chuni_music_list[i].lv[2], score2eval(ex_list[i].score)) + "</td>";
+	scoretable += "<td>" + eval2pdata(chuni_music_list[i].lv[1], score2eval(adv_list[i].score)) + "</td>";
+	scoretable += "<td>" + eval2pdata(chuni_music_list[i].lv[0], score2eval(ba_list[i].score)) + "</td></tr>";
 }
 scoretable += "</table>";
 
