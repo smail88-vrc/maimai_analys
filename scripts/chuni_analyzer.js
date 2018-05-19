8e0987acad0d9f0efb95537bb924b0b09088ec58
Nismo Sgimera <nismo.sgimera@gmail.com>
2018-05-19 21:19:18 +0900

    Update chuni_analyzer.js

--- scripts/chuni_analyzer.js
+++ scripts/chuni_analyzer.js
@@ -4,6 +4,21 @@ javascript:
 var ma_list=[], ex_list=[], adv_list=[], ba_list=[], mname_list=[];
 var chuni_dom='https://chunithm-net.com/mobile/';
 
+function score2eval(score)
+{
+	return  (score>=1010000)?{rank:'SSS', achi:1.00}:
+		(score>=1007500)?{rank:'SSS', achi:(score-1007500)*0.75/ 2500}:
+		(score>=1005000)?{rank:'SS', achi:1+(score-1005000)*0.5 / 2500}:
+		(score>=1000000)?{rank:'SS' , achi:(score-1000000)*0.5 / 5000}:
+		(score>= 975000)?{rank:'S'  , achi:(score- 975000)/ 25000}:
+		(score>= 950000)?{rank:'AAA', achi:(score- 950000)*1.5 /25000}:
+		(score>= 925000)?{rank:'AA' , achi:(score- 925000)*1.5/ 25000}:
+		(score>= 900000)?{rank:'A'  , achi:(score- 900000)*2.0/ 25000}:
+		(score>= 800000)?{rank:'BBB', achi:(score- 800000)/100000}:
+		(score>= 500000)?{rank:'C'  , achi:(score- 500000)/100000}:
+		{rank:'D', achi:score/500000};
+}
+
 function list2data(x)
 {
   var lamplist=[
@@ -15,36 +30,36 @@ function list2data(x)
   ]
   var lamp_rslt=['Clr', 'FC', 'AJ', 'FCh', 'FCh-'];
   var name, score, clr="", fcaj="", fch="", tmp;
-	name=$(x).find('.music_title')[0].innerText.trim();
-	tmp=$(x).find('.text_b');
-	score=(tmp.length!=0)?(Number(tmp[0].innerText.replace(/,/g, ""))):0;
-	lamp=Array.prototype.slice.call($(x).find('img')).map(function(x){return x.getAttribute('src')})
+  name=$(x).find('.music_title')[0].innerText.trim();
+  tmp=$(x).find('.text_b');
+  score=score2eval((tmp.length!=0)?(Number(tmp[0].innerText.replace(/,/g, ""))):0);
+  var lamp=Array.prototype.slice.call($(x).find('img')).map(function(x){return x.getAttribute('src')})
   
-	for(var i=0; i<lamp.length; i++)
+  for(var i=0; i<lamp.length; i++)
+  {
+  	var idx=lamplist.indexOf(lamp[i])
+	switch(idx)
 	{
-		var idx=lamplist.indexOf(lamp[i])
-		switch(idx)
-		{
-			case -1: continue;
-			default: break;
-		}
-		switch(lamp_rslt[idx])
-		{
-			case 'Clr':
-				clr=lamp_rslt[idx];
-				break;
-			case 'FC':
-			case 'AJ':
-				fcaj=lamp_rslt[idx];
-				break;
-			case 'FCh':
-			case 'FCh-':
-				fch=lamp_rslt[idx];
-				break;
-			default: break;
-		}
+		case -1: continue;
+		default: break;
 	}
-	return {score:score, lamp0:clr, lamp1:fcaj, lamp2:fch};
+	switch(lamp_rslt[idx])
+	{
+		case 'Clr':
+			clr=lamp_rslt[idx];
+			break;
+		case 'FC':
+		case 'AJ':
+			fcaj=lamp_rslt[idx];
+			break;
+		case 'FCh':
+		case 'FCh-':
+			fch=lamp_rslt[idx];
+			break;
+		default: break;
+	}
+}
+return {rank:score.rank, achi:score.achi, lamp0:clr, lamp1:fcaj, lamp2:fch};
 }
 
 function get_scoredata(addr, diff, array)
@@ -72,23 +87,11 @@ function get_musicname(addr, diff, array)
 	return;
 }
 
-function score2eval(score)
-{
-	return  (score>=1010000)?{rank:'SSS', achi:1.00}:
-		(score>=1007500)?{rank:'SSS', achi:(score-1007500)*0.75/ 2500}:
-		(score>=1005000)?{rank:'SS', achi:1+(score-1005000)*0.5 / 2500}:
-		(score>=1000000)?{rank:'SS' , achi:(score-1000000)*0.5 / 5000}:
-		(score>= 975000)?{rank:'S'  , achi:(score- 975000)/ 25000}:
-		(score>= 950000)?{rank:'AAA', achi:(score- 950000)*1.5 /25000}:
-		(score>= 925000)?{rank:'AA' , achi:(score- 925000)*1.5/ 25000}:
-		(score>= 900000)?{rank:'A'  , achi:(score- 900000)*2.0/ 25000}:
-		(score>= 800000)?{rank:'BBB', achi:(score- 800000)/100000}:
-		(score>= 500000)?{rank:'C'  , achi:(score- 500000)/100000}:
-		{rank:'D', achi:score/500000};
-}
-
 function eval2pdata(l,d)
 {
+	var tmp ="";
+	
+	tmp += l + '| '
 	switch(d.rank)
 	{
 		case 'SSS':
@@ -97,14 +100,20 @@ function eval2pdata(l,d)
 		case 'AAA':
 		case 'AA':
 		case 'A':
-			return l + '/ ' + d.rank + '+' + (Math.floor(d.achi * 100)/100).toFixed(2);
+			tmp += l + '/ ' + d.rank + '+' + (Math.floor(d.achi * 100)/100).toFixed(2);
+			break;
 		case 'BBB':
 		case 'C':
 		case 'D' :
-			return l + '/ ' + d.rank + '+' + (Math.floor(d.achi *10000)/100).toFixed(2) + '%';
+			tmp += l + '/ ' + d.rank + '+' + (Math.floor(d.achi *10000)/100).toFixed(2) + '%';
+			break;
 		default:
-			return "";
+			break;
 	}
+	if(d.lamp1!="")
+		tmp += '+' + d.lamp1;
+	return tmp;
+
 }
 
 //メインはここから
@@ -121,22 +130,10 @@ for(var i=0; i<mname_list.length; i++)
 {
 	var musicname=(chuni_music_list[i].nick!="")?chuni_music_list[i].nick:chuni_music_list[i].name
 	scoretable += "<tr><th>" + musicname + "</th>";
-	scoretable += "<td>" + eval2pdata(chuni_music_list[i].lv[3], score2eval(ma_list[i].score));
-	if(ma_list[i].lamp1!="")
-		scoretable += '/ +'  + ma_list[i].lamp1;
-	scoretable += "</td>";
-	scoretable += "<td>" + eval2pdata(chuni_music_list[i].lv[2], score2eval(ex_list[i].score));
-	if(ex_list[i].lamp1!="")
-		scoretable += '/ +' + ex_list[i].lamp1;
-	scoretable += "</td>";
-	scoretable += "<td>" + eval2pdata(chuni_music_list[i].lv[1], score2eval(adv_list[i].score));
-	if(adv_list[i].lamp1!="")
-		scoretable += '/ +' + adv_list[i].lamp1;
-	scoretable += "</td>";
-	scoretable += "<td>" + eval2pdata(chuni_music_list[i].lv[0], score2eval(ba_list[i].score));
-	if(ba_list[i].lamp1!="")
-		scoretable += '/ +' + ba_list[i].lamp1;
-	scoretable += "</td>";
+	scoretable += "<td>" + eval2pdata(chuni_music_list[i].lv[3], ma_list[i]) + "</td>";
+	scoretable += "<td>" + eval2pdata(chuni_music_list[i].lv[2], ex_list[i]) + "</td>";
+	scoretable += "<td>" + eval2pdata(chuni_music_list[i].lv[1], adv_list[i]) + "</td>";
+	scoretable += "<td>" + eval2pdata(chuni_music_list[i].lv[0], ba_list[i]) + "</td>";
 	scoretable += "</tr>";
 }
 scoretable += "</table>";
