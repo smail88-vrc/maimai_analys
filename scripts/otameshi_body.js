7c687b37ed0c7b464b8ee4ce4998d5b218bbfae2
Nismo Sgimera <nismo.sgimera@gmail.com>
2018-05-26 20:38:46 +0900

    Update otameshi_body.js

--- scripts/otameshi_body.js
+++ scripts/otameshi_body.js
@@ -842,14 +842,14 @@ function print_lest_comp(ver, background, fontcolor, ma_data, ex_data, music_cou
 	var tmp = "";
 	tmp += "<tr bgcolor=" + background + " align=center>";
 	tmp += "<th rowspan=2><font color='" + fontcolor + "'>" + ver + '(' + music_count + ')' + "</font></th>";
-	tmp += "<td class=mai_master><font color=" + fontcolor + ">M</font></td>";
+	tmp += "<td class=mai_master>M</td>";
 	for(var i=0; i<4; i++)
-		tmp += "<td>" + (music_count - ma_data[i]) + "</td>";
+		tmp += "<td><font color='" + fontcolor + "'>" + (music_count - ma_data[i]) + "</font></td>";
 	tmp += "</tr>";
 	tmp += "<tr bgcolor=" + background + " align=center>";
-	tmp += "<td class=mai_expert><font color=" + fontcolor + ">E</font></td>";
+	tmp += "<td class=mai_expert>E</td>";
 	for(var i=0; i<4; i++)
-		tmp += "<td>" + (music_count - ex_data[i]) + "</td>";
+		tmp += "<td><font color='" + fontcolor + "'>" + (music_count - ex_data[i]) + "</font></td>";
 	tmp += "</tr>";
 	
 	return tmp;
